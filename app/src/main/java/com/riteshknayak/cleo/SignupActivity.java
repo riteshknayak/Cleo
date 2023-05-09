dc908b0f194b2da255f2e598acb7f01eba4a794c
PRORITESH <RITESH.KUMAR.NAYAK.28.10.2005@gmail.com>
2023-05-09 13:24:08 +0530

    Minor changed in code

--- app/src/main/java/com/riteshknayak/cleo/SignupActivity.java
+++ app/src/main/java/com/riteshknayak/cleo/SignupActivity.java
@@ -2,14 +2,26 @@ package com.riteshknayak.cleo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.auth.User;
 import com.riteshknayak.cleo.databinding.ActivitySignupBinding;
+
+import java.util.Objects;
 
 public class SignupActivity extends AppCompatActivity {
 
     ActivitySignupBinding binding;
+
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
